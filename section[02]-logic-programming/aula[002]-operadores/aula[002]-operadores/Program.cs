6688f71a1d93d4b5e589c6edc35535c4a2731363
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-09-25 16:59:25 -0300

    section[02]-aula[002]

---
+++ section[02]-logic-programming/aula[002]-operadores/aula[002]-operadores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_002__operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("soma: 5 + 2 = ");
+            Console.WriteLine(5 + 2);
+
+            Console.Write("soma: 5 - 2 = ");
+            Console.WriteLine(5 - 2);
+
+            Console.Write("soma: 5 * 2 = ");
+            Console.WriteLine(5 * 2);
+
+            Console.Write("soma: 5 / 2 = ");
+            Console.WriteLine(5 / 2);
+
+            Console.Write("soma: 5 % 2 = ");
+            Console.WriteLine(5 % 2);
+
+            Console.Write("soma: 5 > 2 = ");
+            Console.WriteLine(5 > 2);
+
+            Console.Write("soma: 5 >= 2 = ");
+            Console.WriteLine(5 >= 2);
+
+            Console.Write("soma: 5 < 2 = ");
+            Console.WriteLine(5 < 2);
+
+            Console.Write("soma: 5 <= 2 = ");
+            Console.WriteLine(5 <= 2);
+
+            Console.Write("soma: 5 == 2 = ");
+            Console.WriteLine(5 == 2);
+
+            Console.Write("soma: 5 != 2 = ");
+            Console.WriteLine(5 != 2);
+
+            Console.ReadLine();
+        }
+    }
+}
