a27c949bfff620ffc86c2e5251ac4945396c84d0
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-09-27 16:41:15 -0300

    section[02]-aula[004]

---
+++ section[02]-logic-programming/aula[004]-variaveis/aula[004]-variaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_004__variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            text = "testando variaveis";
+
+            Console.WriteLine(text);
+
+            int num1;
+            int num2;
+
+            num1 = 2;
+            num2 = 3;
+
+            Console.WriteLine(num1 + " > " + num2 + " = " + (num1 > num2));
+            Console.WriteLine(num1 + " < " + num2 + " = " + (num1 < num2));
+            Console.WriteLine(num1 + " != " + num2 + " = " + (num1 != num2));
+            Console.WriteLine(num1 + " == " + num2 + " = " + (num1 == num2));
+        }
+    }
+}
