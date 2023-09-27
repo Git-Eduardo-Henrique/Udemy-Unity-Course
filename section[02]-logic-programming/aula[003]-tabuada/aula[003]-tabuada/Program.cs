709870ef3ba63c3388e31ea98e8055ae1d027ec4
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-09-27 16:25:57 -0300

    section[02]-aula[003]-ex

---
+++ section[02]-logic-programming/aula[003]-tabuada/aula[003]-tabuada/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_003__tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tabuada do 2");
+
+            Console.WriteLine("2 x 01 = " + (2 * 1));
+            Console.WriteLine("2 x 02 = " + (2 * 2));
+            Console.WriteLine("2 x 03 = " + (2 * 3));
+            Console.WriteLine("2 x 04 = " + (2 * 4));
+            Console.WriteLine("2 x 05 = " + (2 * 5));
+            Console.WriteLine("2 x 06 = " + (2 * 6));
+            Console.WriteLine("2 x 07 = " + (2 * 7));
+            Console.WriteLine("2 x 08 = " + (2 * 8));
+            Console.WriteLine("2 x 09 = " + (2 * 9));
+            Console.WriteLine("2 x 10 = " + (2 * 10));
+
+            Console.ReadLine();
+        }
+    }
+}
