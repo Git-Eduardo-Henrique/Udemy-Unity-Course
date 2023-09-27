22fc91ffab81b45fcec96c85605c46192664b811
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-09-27 16:54:16 -0300

    section[02]-aula[005]

---
+++ section[02]-logic-programming/aula[005]-imputs/aula[005]-imputs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_005__imputs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.Write("qual o seu nome: ");
+            nome = Console.ReadLine();
+
+            // {0} exibe a variavel, 0 é o indice
+            Console.WriteLine("Olá {0} Seje bem vindo(a)!!", nome);
+            Console.ReadKey();
+        }
+    }
+}
