37deb615c08c33fbd0fd1e06f4336c066775b495
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-09-29 17:04:51 -0300

    section[02]-aula[008]

---
+++ section[02]-logic-programming/aula[008]-divisao/aula[008]-divisao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_008__divisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 0;
+            double num2 = 0;
+            double resto = 0;
+            double quociente = 0;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.Write("digite o primeiro numero: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite o segundo numero: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            resto = num1 % num2;
+            quociente = num1 / num2;
+
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, quociente);
+            Console.WriteLine("resto: {0}", resto);
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+    }
+}
