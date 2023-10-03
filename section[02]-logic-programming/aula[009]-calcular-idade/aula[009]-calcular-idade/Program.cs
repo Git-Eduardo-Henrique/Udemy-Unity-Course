c43090571c77d6f9198394c7ddd198e492d76609
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-03 16:26:55 -0300

    section[02]-aula[009]

---
+++ section[02]-logic-programming/aula[009]-calcular-idade/aula[009]-calcular-idade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_009__calcular_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int atual = DateTime.Now.Year;
+            int nasc = 0;
+            int idade = 0;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.Write("Digite seu ano de nascimento: ");
+            nasc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            idade = atual - nasc;
+            Console.WriteLine("{0} - {1} = {2}", atual, nasc, idade);
+           
+
+            Console.WriteLine("Sua idade atual em {0} é de: {1} anos", atual, idade);
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.ReadKey();
+        }
+    }
+}
