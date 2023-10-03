6e0aedd8cf2bcfdbc7f076797b30ae3ae8ac1b24
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-03 18:00:29 -0300

    section[02]-aula[014]

---
+++ section[02]-logic-programming/aula[014]-emprestimo/aula[014]-emprestimo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_014__emprestimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool empregado = false;
+            bool fiador = false;
+            string entrada = "";
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.Write("Você possui um fiador? (S/N): ");
+            entrada = Console.ReadLine();
+
+            if (entrada == "S") {
+                fiador = true;
+            }
+
+            Console.Write("Você está empregado? (S/N): ");
+            entrada = Console.ReadLine();
+
+            if (entrada == "S")
+            {
+                empregado = true;
+            }
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            if (empregado || fiador) {
+                Console.WriteLine("Seu imprestimo foi APROVADO parabens!!!");
+            } else {
+                Console.WriteLine("Seu imprestimo foi NEGADO!!!");
+            }
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+    }
+}
