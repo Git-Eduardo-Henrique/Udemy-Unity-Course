88d4a0943301f07818a9a5bf9b2f920f52b730c7
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-04 17:04:54 -0300

    section[02]-aula[015]

---
+++ section[02]-logic-programming/aula[015]-veri-eleitor/aula[015]-veri-eleitor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_015__veri_eleitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.Write("Qual sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.WriteLine("Sua idade é de {0} anos.", idade);
+
+            if (idade < 16) {
+                Console.WriteLine("Você NÃO pode votar!!!");
+            } else if ((idade >= 16 && idade < 18) || idade > 65) {
+                Console.WriteLine("Seu voto é OPCIONAL!!!");
+            } else {
+                Console.WriteLine("Seu voto é OBRIGATORIO!!!");
+            } 
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+    }
+}
