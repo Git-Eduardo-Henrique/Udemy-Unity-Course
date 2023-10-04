67da232c3c6f54fb2ce8fccd8c4469c13c192a4c
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-04 17:19:42 -0300

    section[02]-aula[016]

---
+++ section[02]-logic-programming/aula[016]-for/aula[016]-for/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_016__for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, cont = 0;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.Write("Digite um numero para ver sua tabuada: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            for (cont = 1; cont <= 10; cont++) {
+                Console.WriteLine("{0} x {1} = {2}", num, cont, (num * cont));
+            }
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+    }
+}
