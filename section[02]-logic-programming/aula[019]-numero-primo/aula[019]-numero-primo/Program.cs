692d4332620a65ea8552795bba7a8934e1a875f8
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-04 18:05:19 -0300

    section[02]-aula[019]

---
+++ section[02]-logic-programming/aula[019]-numero-primo/aula[019]-numero-primo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_019__numero_primo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, cont = 1, dividiu = 0, exit = 1;
+
+            while (exit != 0) {
+                dividiu = 0;
+          
+                Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+                Console.Write("Digite um numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                for (cont = 1; cont <= num; cont++) {
+                    if (num % cont == 0)
+                      {
+                          dividiu++;
+                      }
+                }
+
+                Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                if (dividiu == 2) {
+                    Console.WriteLine("O numero {0} É PRIMO!", num);
+                }
+                else {
+                    Console.WriteLine("O numero {0} NÃO É PRIMO!", num);
+                }
+                Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                Console.ReadKey();
+
+                Console.Write("Digite 0 para sair: ");
+                exit = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+            }
+        }
+    }
+}
