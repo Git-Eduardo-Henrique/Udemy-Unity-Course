efe78e77b0d79a3a6bfb76816e08e34463578680
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-04 17:37:04 -0300

    section[02]-aula[017]

---
+++ section[02]-logic-programming/aula[017]-media/aula[017]-media/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_017__media
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int user_num = 0, cont = 0;
+            double num = 0, sum = 0, media = 0;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.Write("Quantos numeros deseja informar? ");
+            user_num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            for (cont = 1; cont <= user_num; cont++) {
+                Console.Write("Digite o {0}° numero: ", cont);
+                num = Convert.ToDouble(Console.ReadLine());
+
+                sum += num;
+            }
+
+            media = sum / user_num;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.WriteLine("A media destes {0} numeros é: {1}", user_num, media);
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+    }
+}
