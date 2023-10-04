b11db2bf3d46e4e51f8fd723d98ffa64a4786ca8
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-04 18:52:00 -0300

    section[02]-aula[020]

---
+++ section[02]-logic-programming/aula[020]-lista-nomes/aula[020]-lista-nomes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_020__lista_nomes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lista = new string[5];
+            int cont = 0;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            
+            for (cont = 0; cont < 5; cont++){
+                Console.Write("digite o {0}° nome: ", cont+1, cont);
+                lista[cont] = Console.ReadLine();
+            }
+            
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            for (cont = 0; cont < 5; cont++) {
+                Console.WriteLine("{0}° nome: {1}", cont, lista[cont]);
+            }
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+    }
+}
