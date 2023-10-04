80f0fadc180e13213d00171a71f013dc94aa4e8f
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-04 17:43:30 -0300

    section[02]-aula[018]

---
+++ section[02]-logic-programming/aula[018]-while-tabuada/aula[018]-while-tabuada/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_018__while_tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, cont = 1;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            Console.Write("Digite um numero para ver sua tabuada: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            while (cont <= 10) {
+                Console.WriteLine("{0} x {1} = {2}", num, cont, (num * cont));
+                cont++;
+            }
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+    }
+}
