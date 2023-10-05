604ee6711d36837b9094f5d22d094dff66870044
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-05 17:51:15 -0300

    section[02]-aula[022]

---
+++ section[02]-logic-programming/aula[022]-leitor-de-texto/aula[022]-leitor-de-texto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// biblioteca usada para manipular o texto
+using System.IO;
+
+namespace aula_022__leitor_de_texto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivo = args[0];
+            string line = "";
+
+            StreamReader sr = new StreamReader(arquivo);
+
+            line = sr.ReadLine();
+
+            // quando não tiver linhas para ler ele irá retornar null
+            while (line != null) {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
