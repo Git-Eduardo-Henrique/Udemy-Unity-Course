472d7aac478f1466abf94487a3e9549893a0c9f4
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-08 17:51:39 -0300

    section[02]-projeto-agenda[show-menu]

---
+++ section[02]-logic-programming/projects/projeto-agenda/projeto-agenda/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_agenda
+{
+    internal class Program
+    {
+        public static void ShowData(string[] names, string[] emails) {
+            Console.Clear();
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("nomes: {0}\nemails: {1}", names, emails);
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.ReadKey();
+        }
+        public static int ShowMenu() {
+            Console.Clear();
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("[ 0 ] - Sair\n[ 1 ] - Exibir dados\n[ 2 ] - Adicionar dados");
+            Console.WriteLine("[ 3 ] - Alterar dados\n[ 4 ] - Excluir dados\n[ 5 ] - localizar dados");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.Write("sua opção: ");
+            int opt = Convert.ToInt32(Console.ReadLine());
+
+            return opt;
+        }
+        static void Main(string[] args)
+        {
+            string[] names = new string[200];
+            string[] emails = new string[200];
+
+            int opt = 1;
+
+            while (opt != 0) {
+                opt = ShowMenu();
+
+                switch (opt) {
+                    case 1:
+                        ShowData(names, emails);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+
+        }
+    }
+}
