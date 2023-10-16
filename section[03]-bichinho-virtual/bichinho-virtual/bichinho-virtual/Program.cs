38782c1c213ddaaf9014f9f378e4ee0527cff625
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-10-16 13:51:16 -0300

    section[03]-bichinho-virtual[show-menu]

---
+++ section[03]-bichinho-virtual/bichinho-virtual/bichinho-virtual/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bichinho_virtual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int alimentado = 100;
+            int limpeza = 100;
+            int felicidade = 100;
+
+            int opt = -1;
+
+            if ( name == "") {
+                Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+                Console.Write("Olá meu novo dono :D, Qual o meu nome? ", name);
+                name = Console.ReadLine();
+
+                Console.Clear();
+            }
+            
+            while (true) {
+                opt = in_game.show_menu(name, alimentado, limpeza, felicidade);
+
+                switch (opt) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ section[03]-bichinho-virtual/bichinho-virtual/bichinho-virtual/in-game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bichinho_virtual
+{
+    internal class in_game
+    {
+        public static int show_menu(string name, int alimentado, int limpo, int feliz) {
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("{0}, seu bichinho virtual :D", name);
+            Console.WriteLine("nivel alimentado : {0}\nnivel limpo : {1}", alimentado, limpo);
+            Console.WriteLine("nivel felicidade : {0}", feliz);
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.Write("Opções: [ 0 ] - sair | [ 1 ] - brincar");
+            int opt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+            return opt;
+        }
+    }
+}
