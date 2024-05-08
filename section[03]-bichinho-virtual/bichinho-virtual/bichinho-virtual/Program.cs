0fee642abb454faa13a549309467a36787c15c28
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2024-05-08 16:00:37 -0300

    section[03]-bichinho-virtual[load_data]

--- section[03]-bichinho-virtual/bichinho-virtual/bichinho-virtual/Program.cs
+++ section[03]-bichinho-virtual/bichinho-virtual/bichinho-virtual/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// para coleta de dados
+using System.IO;
 
 namespace bichinho_virtual
 {
@@ -19,12 +21,32 @@ namespace bichinho_virtual
             texts[3] = "Adorei brincar hoje, pena que você não viu";
             texts[4] = "Posso ir na rua? não? :(";
 
-            int alimentado = 100, limpeza = 100, felicidade = 100, opt = -1;
+            in_game.veri_name(ref name);
+
+            int alimentado = 100, limpeza = 100, felicidade = 100;
+
+            string file = "C:\\Users\\Eduardo\\Documents\\GitHub\\Udemy-Unity-Course\\section[03]-bichinho-virtual\\bichinho-virtual\\bichinho-virtual\\" + name + ".txt";
+            if (File.Exists(file))
+            {
+                string[] dados = File.ReadAllLines(file);
+                alimentado = Convert.ToInt32(dados[1]);
+                limpeza = Convert.ToInt32(dados[2]);
+                felicidade = Convert.ToInt32(dados[3]);
+
+                if(alimentado <= 0 || limpeza <= 0 || felicidade <= 0) {
+                    Console.WriteLine("seu bichinho está fraco!!!! vamos recuperalo");
+                    alimentado = 100;
+                    limpeza = 100;
+                    felicidade = 100;
+                }
+            } else {
+                Console.WriteLine("Não foi" + file);
+            }
+
+            int opt = -1;
             bool rodando = true;
 
             Random rand = new Random();
-
-            in_game.veri_name(ref name);
 
             while (rodando) {
                 opt = in_game.show_menu(rand, name, alimentado, limpeza, felicidade, texts);
